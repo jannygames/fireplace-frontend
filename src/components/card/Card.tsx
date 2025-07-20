b75ab6feea4e6c3310ebd4c7cd53ecea8cb53904
jannygames <jamolisdev@gmail.com>
2025-07-20 23:22:34 +0300

    refactor: made card component modular

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,13 +1,15 @@
-const Card = () => {
+const Card = ({title, buttonText, className}: {title: string, buttonText: string, className?: string}) => {
     return (
-        <div className="absolute top-1/2 left-0 ml-20 -translate-y-1/2 w-[30vw] h-[70vh] bg-bg-primary/70 backdrop-blur-sm justify-center items-center justify-items-center">
+        <div className={`absolute ${className} top-1/2 left-0 ml-20 -translate-y-1/2 w-[30vw] h-[70vh] bg-bg-primary/70 backdrop-blur-sm justify-center items-center justify-items-center`}>
             <div className="flex flex-col gap-10 w-[80%] h-full py-15">
                 <h1 className="text-[3.8vw] font-normal text-dark-primary leading-[1.2] tracking-tight select-none">
-                    Aukščiausios kokybės darbai Jūsų namams
+                    {title}
                 </h1>
 
                 <div className="ml-0 mt-auto">
-                    <button className="bg-primary text-white text-2xl uppercase px-4 py-2 w-full h-[60px] hover:bg-primary/80 transition-all duration-300 font-normal tracking-wide">Mūsų darbai</button>
+                    <button className="bg-primary text-white text-2xl uppercase px-4 py-2 w-full h-[60px] hover:bg-primary/80 transition-all duration-300 font-normal tracking-wide">
+                        {buttonText}
+                    </button>
                 </div>
             </div>
         </div>
--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <section className="w-full h-screen bg-bg-primary absolute top-0 overflow-hidden">
         <div className="absolute w-2/3 h-full bg-bg-secondary top-0 left-0" />
         <Slider className="absolute bottom-0 right-0 w-4/5 h-2/3 translate-y-1/2" />
-        <Card />
+        <Card title="Aukščiausios kokybės darbai Jūsų namams" buttonText="Mūsų darbai" />
     </section>
   )
 }
